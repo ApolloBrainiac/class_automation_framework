cd8c40f2ce4d9232de949a07f188e60db638dfb9
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-10 19:08:38 -0400

    Finish refactor of login test to use POM, run clean test

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pages.home.login_page import LoginPage
+import unittest
 
 
-class LoginTest():
+class LoginTest(unittest.TestCase):
 
     def test_validLogin(self):
         baseUrl = "https://letskodeit.teachable.com/"
@@ -11,21 +13,8 @@ class LoginTest():
         driver.implicitly_wait(4)
         driver.get(baseUrl)
 
-        loginLink = driver.find_element(
-            By.LINK_TEXT, "Login")
-        loginLink.click()
-
-        emailField = driver.find_element(
-            By.ID, "user_email")
-        emailField.send_keys("test@email.com")
-
-        passwordField = driver.find_element(
-            By.ID, "user_password")
-        passwordField.send_keys("abcabc")
-
-        loginButton = driver.find_element(
-            By.NAME, "commit")
-        loginButton.click()
+        lp = LoginPage(driver)
+        lp.login("test@email.com", "abcabc")
 
         userIcon = driver.find_element(
             By.XPATH, ".//*[@id='navbar']//span[text()='User Settings']")

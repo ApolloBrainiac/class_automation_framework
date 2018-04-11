5a939f12571282d26cd8875ddd614877446bc2db
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-11 12:43:31 -0400

    Include custom wrappers for ByType and Explicit Wait from prior lessons

---
+++ base/selenium_driver.py
@@ -0,0 +1,87 @@
+from selenium.webdriver.common.by import By
+from traceback import print_stack
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+
+
+class SeleniumDriver():
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def getByType(self, locatorType):
+        locatorType = locatorType.lower()
+        if locatorType == "id":
+            return By.ID
+        elif locatorType == "xpath":
+            return By.XPATH
+        elif locatorType == "name":
+            return By.NAME
+        elif locatorType == "css":
+            return By.CSS_SELECTOR
+        elif locatorType == "classname":
+            return By.CLASS_NAME
+        elif locatorType == "linktext":
+            return By.LINK_TEXT
+        else:
+            print("Locator type " + locatorType + " is not correct/supported")
+        return False
+
+    def getElement(self, locator, locatorType="id"):
+        element = None
+        try:
+            locatorType = locatorType.lower()
+            byType = self.getByType(locatorType)
+            element = self.driver.find_element(byType, locator)
+            print("Element Found")
+        except:
+            print("Element not found")
+            return element
+
+    def isElementPresent(self, locator, byType):
+        try:
+            element = self.driver.find_element(byType, locator)
+            if element is not None:
+                print("Element Found")
+                return True
+            else:
+                print("Element not found")
+                return False
+        except:
+            print("Element not found")
+            return False
+
+    def elementPresenceCheck(self, locator, byType):
+        try:
+            elementList = self.driver.find_elements(byType, locator)
+            if len(elementList) > 0:
+                print("Element Found")
+                return True
+            else:
+                print("Element not found")
+            return False
+        except:
+            print("Element not found")
+            return False
+
+    def waitForElements(self, locator, locatorType="id",
+                        timeout=10, pollFrequency=0.5):
+        element = None
+        try:
+            byType = self.getByType(locatorType)
+            print("Waiting for maximum :: " + str(timeout) +
+                  " :: seconds for elment to be clickable")
+            wait = WebDriverWait(
+                self.driver, 10, poll_frequency=1,
+                ignored_exceptions=[NoSuchElementException,
+                                    ElementNotVisibleException,
+                                    ElementNotSelectableException])
+            element = wait.until(
+                EC.element_to_be_clickable((By.ID, "stopFilter_stops-0")))
+
+            print("Element appeared on the web page")
+        except:
+            print("Element not appeared on the web page")
+            print_stack()
+        return element
